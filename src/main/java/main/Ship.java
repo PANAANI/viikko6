bc2d3ec6d8a907496eaaf1280905953d67c2db19
Joona Niemenmaa <joonaoskari@hotmail.com>
2024-02-14 14:33:07 +0200

    Lisäsin moottorit Ship:n ja Plane:n konstruktoreihin

--- src/main/java/main/Ship.java
+++ src/main/java/main/Ship.java
@@ -3,5 +3,6 @@ package main;
 public class Ship extends Vehicle {
     public Ship(String _manufacturer, String _model, int _maxSpeed) {
         super("laiva", _manufacturer, _model, _maxSpeed);
+        engine = new Engine("Wärtsilä Super", 1000);
     }
 }
