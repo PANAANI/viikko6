49f36da315c4a753366baa2176dbfc227640aeed
Joona Niemenmaa <joonaoskari@hotmail.com>
2024-02-14 14:35:10 +0200

    fuck

--- src/main/java/main/Plane.java
+++ src/main/java/main/Plane.java
@@ -3,5 +3,6 @@ package main;
 public class Plane extends Vehicle {
     public Plane(String _manufacturer, String _model, int _maxSpeed) {
         super("lentokone", _manufacturer, _model, _maxSpeed);
+        engine = new Engine("Suihkumoottori", 500);
     }
 }
